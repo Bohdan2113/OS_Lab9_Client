d76ef9a8fd4adf7154207a5376d527bfd35f1bda
Bohdan <bogdankruk29@gmail.com>
2024-12-05 16:33:08 +0200

    Add timer
    Add timer for creating idea and vote pages

--- OS_Lab9_Client/build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/debug/moc_mainwindow.cpp
+++ OS_Lab9_Client/build/Desktop_x86_windows_msvc2022_pe_64bit-Debug/debug/moc_mainwindow.cpp
@@ -7,6 +7,7 @@
 *****************************************************************************/
 
 #include "../../../mainwindow.h"
+#include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
@@ -41,33 +42,37 @@ constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::string
     "MainWindow",
     "on_joinButton_clicked",
     "",
+    "on_sendButton_clicked",
     "on_quitButton_clicked",
     "on_voteButton_clicked",
     "on_homeButton_2_clicked",
     "on_voteTable_itemSelectionChanged",
-    "FillVoteTable",
-    "OutputPodium",
-    "on_sendButton_clicked",
     "on_nameJoinLineEdit_textChanged",
-    "arg1",
-    "on_newIdeaTextEdit_textChanged"
+    "lineText",
+    "on_newIdeaTextEdit_textChanged",
+    "on_stackedWidget_currentChanged",
+    "pageIndex",
+    "FillVoteTable",
+    "OutputPodium"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[26];
+    uint offsetsAndSizes[30];
     char stringdata0[11];
     char stringdata1[22];
     char stringdata2[1];
     char stringdata3[22];
     char stringdata4[22];
-    char stringdata5[24];
-    char stringdata6[34];
-    char stringdata7[14];
-    char stringdata8[13];
-    char stringdata9[22];
-    char stringdata10[32];
-    char stringdata11[5];
-    char stringdata12[31];
+    char stringdata5[22];
+    char stringdata6[24];
+    char stringdata7[34];
+    char stringdata8[32];
+    char stringdata9[9];
+    char stringdata10[31];
+    char stringdata11[32];
+    char stringdata12[10];
+    char stringdata13[14];
+    char stringdata14[13];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -76,30 +81,34 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
         QT_MOC_LITERAL(0, 10),  // "MainWindow"
         QT_MOC_LITERAL(11, 21),  // "on_joinButton_clicked"
         QT_MOC_LITERAL(33, 0),  // ""
-        QT_MOC_LITERAL(34, 21),  // "on_quitButton_clicked"
-        QT_MOC_LITERAL(56, 21),  // "on_voteButton_clicked"
-        QT_MOC_LITERAL(78, 23),  // "on_homeButton_2_clicked"
-        QT_MOC_LITERAL(102, 33),  // "on_voteTable_itemSelectionCha..."
-        QT_MOC_LITERAL(136, 13),  // "FillVoteTable"
-        QT_MOC_LITERAL(150, 12),  // "OutputPodium"
-        QT_MOC_LITERAL(163, 21),  // "on_sendButton_clicked"
-        QT_MOC_LITERAL(185, 31),  // "on_nameJoinLineEdit_textChanged"
-        QT_MOC_LITERAL(217, 4),  // "arg1"
-        QT_MOC_LITERAL(222, 30)   // "on_newIdeaTextEdit_textChanged"
+        QT_MOC_LITERAL(34, 21),  // "on_sendButton_clicked"
+        QT_MOC_LITERAL(56, 21),  // "on_quitButton_clicked"
+        QT_MOC_LITERAL(78, 21),  // "on_voteButton_clicked"
+        QT_MOC_LITERAL(100, 23),  // "on_homeButton_2_clicked"
+        QT_MOC_LITERAL(124, 33),  // "on_voteTable_itemSelectionCha..."
+        QT_MOC_LITERAL(158, 31),  // "on_nameJoinLineEdit_textChanged"
+        QT_MOC_LITERAL(190, 8),  // "lineText"
+        QT_MOC_LITERAL(199, 30),  // "on_newIdeaTextEdit_textChanged"
+        QT_MOC_LITERAL(230, 31),  // "on_stackedWidget_currentChanged"
+        QT_MOC_LITERAL(262, 9),  // "pageIndex"
+        QT_MOC_LITERAL(272, 13),  // "FillVoteTable"
+        QT_MOC_LITERAL(286, 12)   // "OutputPodium"
     },
     "MainWindow",
     "on_joinButton_clicked",
     "",
+    "on_sendButton_clicked",
     "on_quitButton_clicked",
     "on_voteButton_clicked",
     "on_homeButton_2_clicked",
     "on_voteTable_itemSelectionChanged",
-    "FillVoteTable",
-    "OutputPodium",
-    "on_sendButton_clicked",
     "on_nameJoinLineEdit_textChanged",
-    "arg1",
-    "on_newIdeaTextEdit_textChanged"
+    "lineText",
+    "on_newIdeaTextEdit_textChanged",
+    "on_stackedWidget_currentChanged",
+    "pageIndex",
+    "FillVoteTable",
+    "OutputPodium"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -111,7 +120,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-      10,   14, // methods
+      11,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -119,16 +128,17 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   74,    2, 0x08,    1 /* Private */,
-       3,    0,   75,    2, 0x08,    2 /* Private */,
-       4,    0,   76,    2, 0x08,    3 /* Private */,
-       5,    0,   77,    2, 0x08,    4 /* Private */,
-       6,    0,   78,    2, 0x08,    5 /* Private */,
-       7,    0,   79,    2, 0x08,    6 /* Private */,
-       8,    0,   80,    2, 0x08,    7 /* Private */,
-       9,    0,   81,    2, 0x08,    8 /* Private */,
-      10,    1,   82,    2, 0x08,    9 /* Private */,
-      12,    0,   85,    2, 0x08,   11 /* Private */,
+       1,    0,   80,    2, 0x08,    1 /* Private */,
+       3,    0,   81,    2, 0x08,    2 /* Private */,
+       4,    0,   82,    2, 0x08,    3 /* Private */,
+       5,    0,   83,    2, 0x08,    4 /* Private */,
+       6,    0,   84,    2, 0x08,    5 /* Private */,
+       7,    0,   85,    2, 0x08,    6 /* Private */,
+       8,    1,   86,    2, 0x08,    7 /* Private */,
+      10,    0,   89,    2, 0x08,    9 /* Private */,
+      11,    1,   90,    2, 0x08,   10 /* Private */,
+      13,    0,   93,    2, 0x08,   12 /* Private */,
+      14,    0,   94,    2, 0x08,   13 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -137,9 +147,10 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    9,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   12,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString,   11,
     QMetaType::Void,
 
        0        // eod
@@ -156,6 +167,8 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
         // method 'on_joinButton_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_sendButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_quitButton_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_voteButton_clicked'
@@ -164,16 +177,17 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_voteTable_itemSelectionChanged'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'FillVoteTable'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'OutputPodium'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_sendButton_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_nameJoinLineEdit_textChanged'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
         // method 'on_newIdeaTextEdit_textChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_stackedWidget_currentChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'FillVoteTable'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'OutputPodium'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -186,15 +200,16 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         (void)_t;
         switch (_id) {
         case 0: _t->on_joinButton_clicked(); break;
-        case 1: _t->on_quitButton_clicked(); break;
-        case 2: _t->on_voteButton_clicked(); break;
-        case 3: _t->on_homeButton_2_clicked(); break;
-        case 4: _t->on_voteTable_itemSelectionChanged(); break;
-        case 5: _t->FillVoteTable(); break;
-        case 6: _t->OutputPodium(); break;
-        case 7: _t->on_sendButton_clicked(); break;
-        case 8: _t->on_nameJoinLineEdit_textChanged((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
-        case 9: _t->on_newIdeaTextEdit_textChanged(); break;
+        case 1: _t->on_sendButton_clicked(); break;
+        case 2: _t->on_quitButton_clicked(); break;
+        case 3: _t->on_voteButton_clicked(); break;
+        case 4: _t->on_homeButton_2_clicked(); break;
+        case 5: _t->on_voteTable_itemSelectionChanged(); break;
+        case 6: _t->on_nameJoinLineEdit_textChanged((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 7: _t->on_newIdeaTextEdit_textChanged(); break;
+        case 8: _t->on_stackedWidget_currentChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 9: _t->FillVoteTable(); break;
+        case 10: _t->OutputPodium(); break;
         default: ;
         }
     }
@@ -219,13 +234,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 10)
+        if (_id < 11)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 10;
+        _id -= 11;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 10)
+        if (_id < 11)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 10;
+        _id -= 11;
     }
     return _id;
 }
